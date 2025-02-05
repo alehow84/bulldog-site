11d0f9d9d9fc93e9432d7b324ff8ce2b0be8d083
Alex Howlett <alex.howlett@live.co.uk>
2025-02-05 16:58:53 +0000

    update Navbar for mobile view - contact PageLink added

---
+++ src/components/PageLink.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+export default function PageLink({
+  name,
+  path,
+}: {
+  name: string;
+  path: string;
+}) {
+  return (
+    <div className="flex items-center">
+      <div className="text-sm text-white hover:bg-white hover:text-black rounded transition duration-150 ease-in-out">
+        <Link href={`${path}`}>{name}</Link>
+      </div>
+    </div>
+  );
+}
