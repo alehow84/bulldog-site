3fcbaade039f7d7708271f075a26d1a95ca4e546
Alex Howlett <alex.howlett@live.co.uk>
2025-02-05 16:58:53 +0000

    update Navbar for mobile view - contact PageLink added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,16 @@
 import Burger from "@/components/burgerMenu/Burger";
+import HomeButton from "./static/HomeButton";
+import PageLink from "./PageLink";
+import MobileLogo from "../../public/icons/mobileLogo.svg";
+import MobLogo from "../../public/icons/mobileLogo2.svg";
 
 export default function Navbar() {
   return (
     <>
-      <nav className="p-1 w-screen bg-black">
+      <nav className="flex justify-evenly p-1 w-screen bg-black">
         <Burger />
+        <HomeButton size={80} logo={MobileLogo} />
+        <PageLink path="/contact" name="Contact" />
       </nav>
     </>
   );
