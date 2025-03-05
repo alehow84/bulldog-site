20c55f600a98fe670b46072ac87ec04819efea97
Alex Howlett <alex.howlett@live.co.uk>
2025-03-05 11:28:39 +0000

    update attitude blurb section and add link to about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,13 +19,6 @@ export default function Home() {
           <ProjectTile key={projects[4].id} project={projects[4]} />
           <ProjectTile key={projects[5].id} project={projects[5]} />
         </div>
-        {/* <div className="w-full mb-6 mt-14">
-          <div className="flex justify-end pr-72 ">
-            <div className="flex justify-center w-fit rounded-full p-2 text-xl font-medium text-black hover:bg-slate-800 hover:text-white transition duration-150 ease-in-out">
-              <Link href="/projects">View all Projects</Link>
-            </div>
-          </div>
-        </div> */}
         <div className="w-full flex justify-end mt-14 mb-4">
           <div className="w-1/3">
             <Link href="/projects">
@@ -39,16 +32,28 @@ export default function Home() {
           <div className="w-2/5 h-1 rounded-s-full bg-black mb-14"></div>
         </div>
       </div>
-      <div className="w-full h-[80vh] bg-gray-100 flex flex-col items-center justify-center">
+      <div className="w-full min-h-fit bg-gray-100 flex flex-col items-center justify-center py-14">
         <h2 className="text-3xl text-center font-medium py-5 px-3">
           "Attitude is a small thing that makes a big difference"
         </h2>
         <div className="w-3/5 h-1 bg-slate-700 rounded-full my-4"></div>
         <p className="text-lg w-2/3 text-center py-5">
+          Bulldog Developments & Consultancy is a family run property
+          development business with a breadth of combined skills and experience.
+          One of our directors at the helm has previously won four Queen's
+          Awards; three for international trade and one for innovation. Striving
+          for excellence is baked into our core.
+        </p>
+        <p className="text-lg w-2/3 text-center py-5">
           We believe in giving our best efforts in every project we're working
           on. Our focus is on breathing life into old plots, repurposing to make
           the most out of a space and ensuring a quality result.
         </p>
+        <Link href="/about">
+          <div className="w-fit text-xl font-medium p-2 text-black rounded-full hover:bg-slate-700 hover:text-white hover:font-normal transition duration-150 ease-in-out">
+            Find out more
+          </div>
+        </Link>
       </div>
       <div className="w-full min-h-fit py-16 bg-white flex flex-col items-center justify-center">
         <div className="w-full max-w-screen-xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 px-4">
